e315fe4e67bb9dc843f5c02c9c5c385872907485
Selwin Li <selwinli@csus.edu>
2017-11-12 16:31:54 -0800

    Split Google Sheets API authorization into GoogleSheets

--- Check In/src/main/java/SheetsQuickstart.java
+++ Check In/src/main/java/SheetsQuickstart.java
@@ -2,7 +2,7 @@
 // Todos: Please be sure to update the location of your client_secret.json file & the Googlesheet id before running your program.
 // Author: Doan Nguyen
 // Date: 6/1/17
-import com.google.api.client.auth.oauth2.Credential;
+/*import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
@@ -12,105 +12,30 @@ import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
-import com.google.api.services.sheets.v4.SheetsScopes;
+import com.google.api.services.sheets.v4.SheetsScopes;*/
 import com.google.api.services.sheets.v4.model.*;
 import com.google.api.services.sheets.v4.Sheets;
 
-import java.io.FileInputStream;
+//import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+//import java.io.InputStream;
+//import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
-import java.util.Date;
+//import java.util.Date;
 import java.text.*;
 import java.util.Calendar;
 import java.util.Scanner;
-import java.util.*;
+//import java.util.*;
 import javax.mail.*;
 import javax.mail.internet.*;
-import javax.activation.*;
+//import javax.activation.*;
 import org.json.*;
 
-public class SheetsQuickstart {
-    /** Application name. */
-    private static final String APPLICATION_NAME =
-        "Google Sheets API Java Quickstart";
-
-    /** Directory to store user credentials for this application. */
-    private static final java.io.File DATA_STORE_DIR = new java.io.File(
-        System.getProperty("user.home"), ".credentials//sheets.googleapis.com-java-quickstart.json");
-
-    /** Global instance of the {@link FileDataStoreFactory}. */
-    private static FileDataStoreFactory DATA_STORE_FACTORY;
-
-    /** Global instance of the JSON factory. */
-    private static final JsonFactory JSON_FACTORY =
-        JacksonFactory.getDefaultInstance();
-
-    /** Global instance of the HTTP transport. */
-    private static HttpTransport HTTP_TRANSPORT;
-
-    /** Global instance of the scopes required by this quickstart.
-     *
-     * If modifying these scopes, delete your previously saved credentials
-     * at ~/.credentials/sheets.googleapis.com-java-quickstart.json
-     */
-    private static final List<String> SCOPES =
-        Arrays.asList( SheetsScopes.SPREADSHEETS );
-
-    static {
-        try {
-            HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-            DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
-        } catch (Throwable t) {
-            t.printStackTrace();
-            System.exit(1);
-        }
-    }
-
-    /**
-     * Creates an authorized Credential object.
-     * @return an authorized Credential object.
-     * @throws IOException
-     */
-    public static Credential authorize() throws IOException {
-    	
-        // Load client secrets.
-        // Todo: Change this text to the location where your client_secret.json resided
-        InputStream in = new FileInputStream("client_secret.json");
-            // SheetsQuickstart.class.getResourceAsStream("/client_secret.json");
-        GoogleClientSecrets clientSecrets =
-            GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
-
-        // Build flow and trigger user authorization request.
-        GoogleAuthorizationCodeFlow flow =
-                new GoogleAuthorizationCodeFlow.Builder(
-                        HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-                .setDataStoreFactory(DATA_STORE_FACTORY)
-                .setAccessType("offline")
-                .build();
-        Credential credential = new AuthorizationCodeInstalledApp(
-            flow, new LocalServerReceiver()).authorize("user");
-        System.out.println(
-                "Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
-        return credential;
-    }
-
-    /**
-     * Build and return an authorized Sheets API client service.
-     * @return an authorized Sheets API client service
-     * @throws IOException
-     */
-    public static Sheets getSheetsService() throws IOException {
-        Credential credential = authorize();
-        return new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
-                .setApplicationName(APPLICATION_NAME)
-                .build();
-    }
-    
+public class SheetsQuickstart extends GoogleSheets{
+	
     /**
      * This method will check to see if the student name/id is already listed in the sheet. If the name/id is not found, this 
      * indicates the student has not signed in before and should be added to the first empty row available. If the student is found,
@@ -199,27 +124,6 @@ public class SheetsQuickstart {
             kb.close();
             }
         return rowNum;
-    }
-    
-    /**
-     * Finds and returns the current column of the date
-     * @return column
-     */
-    public static int getColumn() throws IOException{
-        // Build a new authorized API client service.
-        Sheets service = getSheetsService();
-        
-        String spreadsheetId = "1A-WnepO4dK77xY4AmFU53PnTCxDwpJdkMpXqXYHgAxQ";
-        
-        String range = "A1:Z1";
-        
-        ValueRange result = service.spreadsheets().values().get(spreadsheetId, range).setMajorDimension("COLUMNS").execute();
-        //result.setMajorDimension("COLUMNS");
-        int column = result.getValues() != null ? result.getValues().size() : 0;
-        //System.out.println(result);
-        //System.out.println(numRows);
-    	//curColumn = 3;
-    	return column;
     }
     
     /**
