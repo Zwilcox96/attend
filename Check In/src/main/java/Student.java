94f6b0914c3122c8c64b7c01c22013d7df412c66
Zack Wilcox <zwilcox96@gmail.com>
2017-11-12 17:07:19 -0800

    Added student Class

---
+++ Check In/src/main/java/Student.java
@@ -0,0 +1,20 @@
+/*
+ * This class is used to create and manipulate students
+ * By Zack Wilcox
+ * ToDo: add required methods, and constructors. Figure out SQL stuff.
+ */
+public class Student {
+	private static int studentID;
+	private static String name;
+	private static String email;
+	
+	
+	
+	public static String getEmail(){
+		return email;
+	}
+	
+	public static String getName(){
+		return name;
+	}
+}
