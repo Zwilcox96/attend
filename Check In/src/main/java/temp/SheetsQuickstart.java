4cddbde622abc19186ddf4794079c47db56cae15
selwinli <selwinli@csus.edu>
2017-11-20 01:05:29 -0800

    ServletApp running

---
+++ Check In/src/main/java/temp/SheetsQuickstart.java
@@ -0,0 +1,135 @@
+package temp;
+// This is SheetsQuickstart. This program is used as a demo to demostrate an update to a fix cell location on a Google sheet
+// Todos: Please be sure to update the location of your client_secret.json file & the Googlesheet id before running your program.
+// Author: Doan Nguyen
+// Date: 6/1/17
+
+import java.io.IOException;
+import java.util.Properties;
+import java.util.Scanner;
+import javax.mail.*;
+import javax.mail.internet.*;
+
+
+public class SheetsQuickstart extends GoogleSheets{
+	
+    
+    
+    /**
+     * This code was borrowed from https://stackoverflow.com/questions/37495808/java-send-email-via-gmail
+     * @param email
+     * @param name
+     */
+    public static void sendReceipt(String email, String subject, String realMessage) {
+    	String to = email;  
+
+    	//Get the session object  
+    	Properties props = new Properties();
+    	props.put("mail.smtp.host", "smtp.gmail.com");
+    	props.put("mail.smtp.socketFactory.port", "465");
+    	props.put("mail.smtp.socketFactory.class",
+    			"javax.net.ssl.SSLSocketFactory");
+    	props.put("mail.smtp.auth", "true");
+    	props.put("mail.smtp.port", "465");
+
+    	Session session = Session.getDefaultInstance(props,
+            new javax.mail.Authenticator() {
+    			protected PasswordAuthentication getPasswordAuthentication() {
+    				return new PasswordAuthentication("agonz08@gmail.com", "extzhdzabitmjmhm");//change accordingly  
+    			}
+    		});
+    	//compose message  
+    	try {
+    		MimeMessage message = new MimeMessage(session);
+    		message.setFrom(new InternetAddress("ecs@ecs.edu"));//change accordingly  
+    		message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+    		message.setSubject(subject);
+    		message.setText(realMessage);
+    		//send message  
+    		Transport.send(message);
+    		System.out.println("message sent successfully");
+    	} catch (MessagingException e) {
+    		throw new RuntimeException(e);
+    	}
+    }
+            
+   
+    
+    public static void main(String[] args) throws IOException {
+        
+        Scanner kb = new Scanner(System.in);
+        System.out.println("Please enter your name:");
+        String name = kb.nextLine();
+        
+        if (name.equals("Professor")) {
+        	setPin();
+        } else if(name.equals("reg")) {
+        	Register.createInstructor(1234, "test@test.edu", "Tester Testersmith", "hi mom");
+        	Instructor i = new Instructor(1234);
+        	String def = i.getEmail();
+        	System.out.println(def);
+        	/*
+        	Student s = new Student(1234);
+        	int abc = s.getSID();
+        	System.out.println(abc);
+        	
+        	Course c = new Course(5555);
+        	Student s = new Student(1234);
+        	Instructor hi = new Instructor(1234);
+        	Course[] sc = s.getCourses();
+        	Course[] hic = s.getCourses();
+        	System.out.println(sc[0].getCourseName());
+        	System.out.println(hic[0].getCourseName());
+        	*/
+        } else {
+            System.out.println("Please enter the PIN for todays class:");
+            String studentPin = kb.nextLine();
+            if (studentPin.equals(getPin()) ) {
+            	int row = checkStudent(name);
+                markAttendance(row, name);
+            } else {
+            	System.out.println("You have entered an incorrect PIN!");
+            }
+        }   
+        kb.close();
+    }
+    public static void updateSheets() throws IOException {
+    	System.out.println("Working Directory = " +
+                System.getProperty("user.dir"));
+        Scanner kb = new Scanner(System.in);
+        System.out.println("Please enter your name:");
+        String name = kb.nextLine();
+        
+        if (name.equals("Professor")) {
+        	setPin();
+        } else if(name.equals("reg")) {
+        	Register.createInstructor(1234, "test@test.edu", "Tester Testersmith", "hi mom");
+        	Instructor i = new Instructor(1234);
+        	String def = i.getEmail();
+        	System.out.println(def);
+        	/*
+        	Student s = new Student(1234);
+        	int abc = s.getSID();
+        	System.out.println(abc);
+        	
+        	Course c = new Course(5555);
+        	Student s = new Student(1234);
+        	Instructor hi = new Instructor(1234);
+        	Course[] sc = s.getCourses();
+        	Course[] hic = s.getCourses();
+        	System.out.println(sc[0].getCourseName());
+        	System.out.println(hic[0].getCourseName());
+        	*/
+        } else {
+            System.out.println("Please enter the PIN for todays class:");
+            String studentPin = kb.nextLine();
+            if (studentPin.equals(getPin()) ) {
+            	int row = checkStudent(name);
+                markAttendance(row, name);
+            } else {
+            	System.out.println("You have entered an incorrect PIN!");
+            }
+        }   
+        kb.close();
+    }
+}
---
+++ Check In/src/main/java/temp/Student.java
@@ -0,0 +1,175 @@
+package temp;
+/*
+ * This class is used to create and manipulate students
+ * By Zack Wilcox
+ * ToDo: add error catching
+ */
+
+import java.sql.*;
+public class Student {
+	private static int studentID;
+	private static String name;
+	private static String email;
+	
+	//These variables are for the database connection
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+	static final String DB_URL = "jdbc:mysql://attend.ctddylfx9obm.us-west-1.rds.amazonaws.com:3306/attend?autoReconnect=true&useSSL=false";
+	static final String USER = "letsstore";
+	static final String PASS = "iloveherky";
+	
+	
+	/**
+	 * This constructor takes a Student's ID number and creates a Student with info from our database.
+	 * JDBC code is modified from code originally found on tutorialspoint.com
+	 * @param sID The ID number of the Student.
+	 */
+	Student(int sID){
+		studentID = sID;
+		 Connection conn = null;
+		 Statement stmt = null;
+		   try{
+		      //STEP 2: Register JDBC driver
+		      Class.forName("com.mysql.jdbc.Driver");
+
+		      //STEP 3: Open a connection
+		      //System.out.println("Connecting to database...");
+		      conn = DriverManager.getConnection(DB_URL,USER,PASS);
+
+		      //STEP 4: Execute a query
+		      //System.out.println("Creating statement...");
+		      stmt = conn.createStatement();
+		      String sql;
+		      sql = "SELECT SID, SName, SEmail FROM student " + "WHERE SID = "  + sID;
+		      ResultSet rs = stmt.executeQuery(sql);
+
+		      //STEP 5: Extract data from result set
+		      while(rs.next()){
+		         //Retrieve by column name
+		         name = rs.getString("SName");
+		         email = rs.getString("SEmail");
+
+		         //Display values
+		         System.out.print("SID: " + sID);
+		         System.out.print(", Name: " + name);
+		         System.out.println(", Email: " + email);
+		      }
+		      //STEP 6: Clean-up environment
+		      rs.close();
+		      stmt.close();
+		      conn.close();
+		   }catch(SQLException se){
+		      //Handle errors for JDBC
+		      se.printStackTrace();
+		   }catch(Exception e){
+		      //Handle errors for Class.forName
+		      e.printStackTrace();
+		   }finally{
+		      //finally block used to close resources
+		      try{
+		         if(stmt!=null)
+		            stmt.close();
+		      }catch(SQLException se2){
+		      }// nothing we can do
+		      try{
+		         if(conn!=null)
+		            conn.close();
+		      }catch(SQLException se){
+		         se.printStackTrace();
+		      }//end finally try
+		   }//end try
+		   //System.out.println("Goodbye!");
+		}//end main
+		
+	/**
+	 * This method finds all of the courses s student is taking 
+	 * and returns them in an array.
+	 * @return An array of courses that this student takes.
+	 */
+	public static Course[] getCourses(){
+		Connection conn = null;
+		 Statement stmt = null;
+		   try{
+		      //STEP 2: Register JDBC driver
+		      Class.forName("com.mysql.jdbc.Driver");
+
+		      //STEP 3: Open a connection
+		      //System.out.println("Connecting to database...");
+		      conn = DriverManager.getConnection(DB_URL,USER,PASS);
+
+		      //STEP 4: Execute a query
+		      //System.out.println("Creating statement...");
+		      stmt = conn.createStatement();
+		      String sql;
+		      sql = "SELECT CallNumber FROM enrolled " + "WHERE SID = "  + studentID;
+		      ResultSet rs = stmt.executeQuery(sql);
+
+		      //STEP 5: Extract data from result set and place in an array
+		      //Find the size of the required array
+		      int size=0;
+		      while (rs.next()) {
+		          size++;
+		      }
+		      //Create array for courses and reset the result list pointer.
+		      Course[] courses = new Course[size];
+		      rs.first();		  
+		      //Fill Array with courses
+		      int counter = 0;
+		      while(rs.next()){
+		         //Retrieve by column name
+		         Course c = new Course(rs.getInt("CallNumber"));
+		         courses[counter] = c;
+		      }
+		      //STEP 6: Clean-up environment
+		      rs.close();
+		      stmt.close();
+		      conn.close();
+		      return courses;
+		   }catch(SQLException se){
+		      //Handle errors for JDBC
+		      se.printStackTrace();
+		   }catch(Exception e){
+		      //Handle errors for Class.forName
+		      e.printStackTrace();
+		   }finally{
+		      //finally block used to close resources
+		      try{
+		         if(stmt!=null)
+		            stmt.close();
+		      }catch(SQLException se2){
+		      }// nothing we can do
+		      try{
+		         if(conn!=null)
+		            conn.close();
+		      }catch(SQLException se){
+		         se.printStackTrace();
+		      }//end finally try
+		      
+		   }//end try
+		return null;
+		   
+	}
+	
+	/**
+	 * This method will give you the email of a Student.
+	 * @return The email of a Student.
+	 */
+	public static String getEmail(){
+		return email;
+	}
+	
+	/**
+	 * This method will give you the name of a student
+	 * @return The name of a Student.
+	 */
+	public static String getName(){
+		return name;
+	}
+	
+	/**
+	 * This method will give you the SID of a Student.
+	 * @return The SID of a Student.
+	 */
+	public static int getSID(){
+		return studentID;
+	}
+}
